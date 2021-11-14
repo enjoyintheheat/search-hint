4a25f042a1a7164ab731c70ac30fc8a5bc956f72
enjoyintheheat <enjoyintheheat@gmail.com>
2021-11-14 14:07:16 +0300

    Feature: add front

--- search_hint/main.py
+++ search_hint/main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI, Request
+from pathlib import Path
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from search_hint.common.websocket import asgi
 from search_hint.celery import celery
+from search_hint.common.settings import static_dir, templates_dir
 
 
 app = FastAPI()
 
 celery_app = celery
 
-app.mount('/', asgi)
+app.mount('/ws', asgi)
+
+app.mount(str(static_dir), StaticFiles(directory=static_dir), name="static")
+
+templates = Jinja2Templates(directory=str(templates_dir))
 
 
-@app.get('/')
-async def index(request: Request):
-    return {'check': True}
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("index.html", {'request': request})
--- search_hint/tasks/recognize.py
+++ search_hint/tasks/recognize.py
@@ -1,14 +1,15 @@
 import asyncio
 import search_hint.common.websocket
 from search_hint.celery import celery
-from search_hint.modules.hint.process import text_processor_factory
+# from search_hint.modules.hint.process import text_processor_factory
 
 
 @celery.task()
 def recognize(text: str, event: str):
-    processor = text_processor_factory()
+    # processor = text_processor_factory()
 
-    output = processor.recognize(text)
+    # output = processor.recognize(text)
+    output = ''
 
     loop = asyncio.get_event_loop()
 
