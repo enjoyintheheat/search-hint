fe07e6b540af5e78b67c95b7ad402b0334605b23
enjoyintheheat <enjoyintheheat@gmail.com>
2021-11-14 16:01:25 +0300

    Refactor: add cors

--- search_hint/main.py
+++ search_hint/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from pathlib import Path
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from search_hint.common.websocket import asgi
@@ -17,6 +18,16 @@ app.mount('/ws', asgi)
 app.mount(str(static_dir), StaticFiles(directory=static_dir), name="static")
 
 templates = Jinja2Templates(directory=str(templates_dir))
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", response_class=HTMLResponse)
